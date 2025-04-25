84e49752fc77a430e710c23c2486b6a4d63a8aa2
Dangcong26 <24021396@vnu.edu.vn>
2025-04-25 23:38:58 +0700

    THEM DU LIEU HINH ANH , AM THANH

---
+++ Sound.h
@@ -0,0 +1,12 @@
+﻿#ifndef SOUND_MANAGER_H
+#define SOUND_MANAGER_H
+
+#include <SDL_mixer.h>
+
+extern Mix_Music* g_background_music; 
+extern Mix_Chunk* g_bullet_hit_sound; 
+extern Mix_Chunk* g_eat_brain_sound;
+extern Mix_Chunk* g_hit_threat_sound;
+extern Mix_Chunk* g_hit_boom_sound;
+
+#endif 
